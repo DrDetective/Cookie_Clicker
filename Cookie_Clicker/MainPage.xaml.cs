b8f36def724ecc57b89148cd0efa1504a8ea4d3e
Fon <bastici30@gmail.com>
2023-04-24 20:53:44 +0200

    Přidejte soubory projektu.

---
+++ Cookie_Clicker/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace Cookie_Clicker;
+
+public partial class MainPage : ContentPage
+{
+	float count = 0f;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+	}
+
+	private void btnC_Clicked(object sender, EventArgs e)
+	{
+		count++;
+		if (count == 1)
+			totalCs.Text = $"{count} cookie";
+		else
+			totalCs.Text = $"{count} cookies";
+		
+		//if (count == 1)
+		//	perSecCs.Text = $"{count / 100} cookie per second";
+		//else
+		//	perSecCs.Text = $"{count / 100} cookies per second";
+
+		SemanticScreenReader.Announce(perSecCs.Text);
+		//SemanticScreenReader.Announce(btnC.Text);
+	}
+    private void btnStats_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnInfo_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
+
---
+++ Cookie_Clicker/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Cookie_Clicker;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
