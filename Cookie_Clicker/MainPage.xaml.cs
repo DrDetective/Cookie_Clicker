a55e0b9aaf30464841db5955cee327bf04af3377
Štěpán Bast <stepan.bast@educhem.cz>
2023-04-25 09:21:29 +0200

    ver 1.1.0

--- Cookie_Clicker/MainPage.xaml.cs
+++ Cookie_Clicker/MainPage.xaml.cs
@@ -26,14 +26,22 @@ public partial class MainPage : ContentPage
 		SemanticScreenReader.Announce(perSecCs.Text);
 		//SemanticScreenReader.Announce(btnC.Text);
 	}
-    private void btnStats_Clicked(object sender, EventArgs e)
+    private async void btnStats_Clicked(object sender, EventArgs e)
     {
+        //DisplayAlert($"Total cookies: {totalCs}", "OK");
+        string action = await DisplayActionSheet("Stats", "Back", null, $"Total cookies: {totalCs.Text}",$"Total clicked cookies: {count.ToString()} cookies", $"Total clicks: {Convert.ToString(count)}", "Runtime: ");
 
     }
 
     private void btnInfo_Clicked(object sender, EventArgs e)
     {
-
+		string info = "";
+    }
+    private async void btnChangeName_Clicked(object sender, EventArgs e)
+    {
+		string change = await DisplayPromptAsync("Change name of your bakery","Your name: ");
+		nameOfBakery.Text =$"{change}'s bakery";
     }
 }
 
+//https://learn.microsoft.com/en-us/dotnet/maui/xaml/fundamentals/essential-syntax?view=net-maui-7.0
